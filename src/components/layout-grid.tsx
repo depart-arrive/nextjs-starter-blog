4074cff04dc6f16e45b8045dd9add85c7e69ee88
depart-arrive <83715816+depart-arrive@users.noreply.github.com>
2021-06-28 16:10:19 +0100

    Update layout-grid.tsx

--- src/components/layout-grid.tsx
+++ src/components/layout-grid.tsx
@@ -8,7 +8,7 @@ export function LayoutGrid({ children, ...props }: LayoutGridProps) {
   return (
     <div
       display="grid"
-      gridTemplateColumns="[full-start] minmax(5vw,auto) [wide-start] minmax(auto,80px) [main-start] min(638px,calc(100% - 10vw)) [main-end] minmax(auto,80px) [wide-end] minmax(5vw,auto) [full-end]"
+      gridTemplateColumns="[full-start] minmax(5vw,auto) [wide-start] minmax(auto,80px) [main-start] min(638px,calc(100% - 10vw)) [main-end] minmax(auto,80px) [wide-end] minmax(auto,5vw) [full-end]"
       py="6|10|12"
       sx={{
         "> *": {
